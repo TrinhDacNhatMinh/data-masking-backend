9eb397c6abf44517626e05769fc857038373ef12
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2026-03-24 21:55:53 +0700

    feat: initial project setup with Spring Boot + Oracle + Docker

---
+++ src/test/java/com/minh/data_masking/DataMaskingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.data_masking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMaskingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
